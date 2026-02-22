a6142369dd8ed7201f1e1e1f997997c8422a7092
Thillaivanan-GG <ggthillaivanan@gmail.com>
2026-02-22 16:55:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -171,4 +171,16 @@ async def predict(file: UploadFile = File(...)):
             "treatment": treat,
             "fertilizer": fert
         }
+
     }
+# =====================================================
+# RUN SERVER (RENDER PORT FIX)
+# =====================================================
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
